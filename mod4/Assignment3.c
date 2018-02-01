0b5084d6b4d4f6c4d5dc682d28e464d2b8474f86
Gabe Cerritos <gabrielcerritos@mail.weber.edu>
2018-02-01 10:17:36 -0700

    Assignment 3 Initial commit

---
+++ mod4/Assignment3.c
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Assignment3.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/01/2018 10:16:42 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gabe Cerritos (), gabrielcerritos@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    char autoService[21];
+       char oil[] = "oil change";
+          char tire[] = "tire rotation";
+             char wash[] = "car wash";
+
+                printf("Oil change --\t$35\n");
+                   printf("Tire rotation --\t$19\n");
+                      printf("Car wash --\t%7");
+
+                         printf("Enter your desired auto service:\n");
+                            scanf("%s", autoService);
+                               
+                               if (tolower(autoService) == 
+                                          
+                                          printf("You entered: %s");
+
+    return 0;
+}
+// Function Definitions
+
+
