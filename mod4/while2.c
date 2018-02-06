ba0217f7fecf08ced5ec0725b83b5d7986da6137
Gabe Cerritos <gabrielcerritos@mail.weber.edu>
2018-02-06 09:27:05 -0700

    Initial commit

---
+++ mod4/while2.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  while2.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/06/2018 09:25:06 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gabe Cerritos (), gabrielcerritos@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int num = 2;
+    
+    while (num) {
+        printf("Num [%d]\n", num);
+    }
+
+    return 0;
+}
+// Function Definitions
+
+
