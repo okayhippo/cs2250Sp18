77cd516ebff2f4ba6f398a24032e3ed83119cdb3
Gabe Cerritos <gabrielcerritos@mail.weber.edu>
2018-03-20 20:20:43 -0600

    Initial commit of ch6 warmup

---
+++ mod8/ch6_warmup.c
@@ -0,0 +1,57 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ch6_warmup.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/20/2018 07:29:10 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gabe Cerritos (), gabrielcerritos@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Constants
+const int MAX = 256;
+
+// Function Prototypes
+int GetNumOfCharacters(char*);
+void OutputWithoutWhitespace(char*);
+
+// Main Function
+int main()
+{
+    char userString[MAX];
+
+    printf("Enter a sentence or phrase: ");
+    fgets(userString, MAX, stdin);
+
+    printf("\nYou entered: %s\n", userString);
+    printf("Number of characters: %d\n", GetNumOfCharacters(userString));
+    OutputWithoutWhitespace(userString);
+
+    return 0;
+}
+
+// Function Definitions
+int GetNumOfCharacters(char* u) {
+    return strlen(u) - 1;
+}
+
+void OutputWithoutWhitespace(char* userStr) {
+    for (int i = 0; i < strlen(userStr); i++) {
+        if (isspace(userStr[i])) {
+            userStr[i] = userStr[i + 1];
+        }
+    }
+    
+    printf("String with no whitespace: %s\n", userStr);
+}
